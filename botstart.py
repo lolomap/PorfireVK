e85c6ea56ffee1c45c608ce5bd697330f3f95d0c
lolomap <lolodegsss@gmail.com>
2020-02-22 13:17:49 +0300

    Запилил фичу, чтобы бот продолжал генерацию в отредактиованном сообщении

--- botstart.py
+++ botstart.py
@@ -5,26 +5,41 @@ from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 import requests
 import json
 
-def WriteMsg(session, event, text):
-    session.messages.send(
-        peer_id = event.obj['peer_id'],
-        random_id = event.obj['random_id'],
-        message = text
+group_token = ''
+group_id = 192176246
+lastBotMsgID = 0
+
+
+def write_msg(session, session_event, text):
+    bot_msg = session.messages.send(
+        peer_id=session_event.obj['peer_id'],
+        random_id=session_event.obj['random_id'],
+        message=text
+    )
+    print(bot_msg)
+    return bot_msg
+
+
+def edit_msg(session, session_event, msg_id):
+    last_msg = session.messages.getById(message_ids=lastBotMsgID)
+    print(last_msg)
+    edit_text = send_request(last_msg['items'][0]['text'])
+    session.messages.edit(
+        peer_id=session_event.obj['peer_id'],
+        message=edit_text,
+        message_id=msg_id
     )
 
 
-def SendRequest(send_text, session, event):
+def send_request(text):
     url = 'https://models.dobro.ai/gpt2/medium/'
-    header = {'Content-Type': 'application/json',
-               'User_Agent': 'Chrome'}
-    payload = {'prompt':send_text, 'length':30, 'num_samples':4}
+    header = {'Content-Type': 'application/json', 'User_Agent': 'Chrome'}
+    payload = {'prompt': send_text, 'length': 30, 'num_samples': 4}
     res = requests.post(url, data=json.dumps(payload), headers=header)
     sub_res = res.json()['replies'][0]
     print(sub_res)
-    WriteMsg(session, event, send_text+sub_res)
+    return text+sub_res
 
-group_token = ''
-group_id = 192176246
 
 while True:
     try:
@@ -40,8 +55,11 @@ while True:
                 msg_text = event.obj['text']
                 if msg_text.find('Прф, ') == 0 or msg_text.find('прф, ') == 0:
                     send_text = msg_text[5:]
+                    lastBotMsgID = write_msg(vkK, event, send_request(send_text))
+                    print(lastBotMsgID)
+                elif msg_text.find('Прф дальше') == 0 or msg_text.find('прф дальше') == 0:
+                    print('Continue')
+                    edit_msg(vkK, event, lastBotMsgID)
 
-                    SendRequest(send_text, vkK, event)
-    except:
-        continue
-
+    except Exception as e:
+        print(e.__class__)
