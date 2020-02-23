3f92a8fc478f6808ad78ea91347226b24f24bf49
lolomap <lolodegsss@gmail.com>
2020-02-23 15:00:33 +0300

    фикс utf

--- MESSAGES_TYPES.py
+++ MESSAGES_TYPES.py
@@ -2,17 +2,17 @@
 
 import API
 
-MSG_GENERATE = ['Прф, ', 'прф, ', 'прф,', 'Прф,', 'P ', 'p ', 'P, ', 'p, ', 'p,', 'P,']
-MSG_NEXT_GENERATE = ['Прф дальше', 'прф дальше', 'p n', 'P n', 'P N']
+MSG_GENERATE = [u'Прф, ', u'прф, ', u'прф,', u'Прф,', u'P ', u'p ', u'P, ', u'p, ', u'p,', u'P,']
+MSG_NEXT_GENERATE = [u'Прф дальше', u'прф дальше', u'p n', u'P n', u'P N']
 
 
 def easter_egg_request(session, session_event):
     if session_event.obj['text']:
         msg = session_event.obj['text'].lower()
-        if msg == 'ты живой?':
-            API.write_msg(session, session_event, 'ПНХ')
-        elif msg.find('запутина') >= 0 or msg.find('коз') >= 0:
-            API.write_msg(session, session_event, 'МЫ ЗА ЗАПУТИНА! СЛАВА ГЕНСЕКУ! СМЕРТЬ ЕДРОСАМ!')
+        if msg == u'ты живой?':
+            API.write_msg(session, session_event, u'ПНХ')
+        elif msg.find(u'запутина') >= 0 or msg.find(u'коз') >= 0:
+            API.write_msg(session, session_event, u'МЫ ЗА ЗАПУТИНА! СЛАВА ГЕНСЕКУ! СМЕРТЬ ЕДРОСАМ!')
         else:
             pass
     if len(session_event.obj['attachments']):
